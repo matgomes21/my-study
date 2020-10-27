c63063af35a9305c16a1d2ccb12ce6b1f1bcb44c
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2020-10-26 21:21:25 -0300

    Remove desnecessary print

--- eda2/sort/insertionsort.c
+++ eda2/sort/insertionsort.c
@@ -9,7 +9,6 @@
 void insertionSort(int *v, int l, int r){
     int k=1;
     for(int i=r;i>l;i--)cmpexch(v[i-1],v[i]);
-    printf("%d",v[0]);
     for(int i=l+2;i<=r;i++){
         int j=i;
         int temp=v[j];
